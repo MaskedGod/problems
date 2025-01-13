b1e7211c4ae9455d866bf98d5a11f60f5812a46c
MaskedGod <codegobbledygook@gmail.com>
2025-01-13 22:56:56 +0400

    2, linked list

--- leetcode/2_add_two_numbers.py
+++ leetcode/2_add_two_numbers.py
@@ -36,13 +36,68 @@ def display_linked_list(lst):
     print("None")
 
 
-a1 = list_to_linked_list([2, 4, 3])
-b1 = list_to_linked_list([5, 6, 4])
+a1 = list_to_linked_list([1, 8])
+b1 = list_to_linked_list([0])
+# a1 = list_to_linked_list([2, 4, 3])
+# b1 = list_to_linked_list([5, 6, 4])
+# a1 = ListNode(2)
+# a2 = ListNode(4)
+# a3 = ListNode(3)
+# a1.next = a2
+# a2.next = a3
+
+# b1 = ListNode(5)
+# b2 = ListNode(6)
+# b3 = ListNode(4)
+# b1.next = b2
+# b2.next = b3
 
 
-def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
-    pass
+def count_nodes(head: ListNode) -> int:
+    counter = 0
+    current = head
+    while current:
+        counter += 1
+        current = current.next
+    return counter
 
 
-print(addTwoNumbers(a1, b1))
-# Traverse and print the linked list to verify
+def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
+    dummy_head = ListNode(0)
+    tail = dummy_head
+    carry = 0
+
+    while l1 or l2 or carry != 0:
+        digit1 = l1.val if l1 else 0
+        digit2 = l2.val if l2 else 0
+
+        nodes_sum = digit1 + digit2 + carry
+        digit = nodes_sum % 10
+        carry = nodes_sum // 10
+
+        tail.next = ListNode(digit)
+        tail = tail.next
+
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+
+    return dummy_head.next
+
+
+print(display_linked_list(add_two_numbers(a1, b1)))
+
+# def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
+#    carry = 0
+#     l3 = ListNode()
+#     dummy = l3
+#     while l1 or l2 or carry:
+#         if l1:
+#             carry+=l1.val
+#             l1 = l1.next
+#         if l2:
+#             carry += l2.val
+#             l2 = l2.next
+#         digit, carry = carry%10, carry//10
+#         l3.next = ListNode(digit)
+#         l3 = l3.next
+#     return dummy.next
